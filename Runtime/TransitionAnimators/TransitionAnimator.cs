99335e84b8ba6d8aff5f5390d7edb80313d545fb
HexTecGames <Patrick.Hellwig93@gmail.com>
2026-01-03 02:29:41 +0100

    fixes

--- Runtime/TransitionAnimators/TransitionAnimator.cs
+++ Runtime/TransitionAnimators/TransitionAnimator.cs
@@ -5,6 +5,12 @@ namespace HexTecGames.TransitionSystem
 {
     public abstract class TransitionAnimator : MonoBehaviour
     {
+        public void ActivateAndPlay(bool reverse = false)
+        {
+            gameObject.SetActive(true);
+            StartCoroutine(Play(reverse));
+        }
+
         public abstract IEnumerator Play(bool reverse = false);
     }
 }
